1c47fc62097e7b14234e99efe8cdb463a4fd1776
Bernt Andreas Johnsen <berntj@viken.no>
2023-05-09 16:33:13 +0200

    Add initial model classes

---
+++ src/main/java/no/fintlabs/assignment/FlattenedAssignment.java
@@ -0,0 +1,27 @@
+package no.fintlabs.assignment;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Slf4j
+@Entity
+@Table(name="FlattenedAssignments")
+@AllArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PUBLIC, force=true)
+public class FlattenedAssignment {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userObjectId;
+    private String groupObjectId;
+    private Long assignmentId;
+}
